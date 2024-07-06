f8356e0793bd9c2de0792e827ff655cc4fe3a304
neera <neerajwadhwaney2003@gmail.com>
2024-07-06 12:55:10 +0530

    29-06-224

---
+++ src/inversionOfControl/Ex1/app/UseStudent.java
@@ -0,0 +1,14 @@
+package inversionOfControl.Ex1.app;
+
+import inversionOfControl.Ex1.beans.Student;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class UseStudent {
+    public static void main(String[] args) {
+        BeanFactory container = new XmlBeanFactory(new ClassPathResource("/inversionOfControl/Ex1/resources/beanConfig.xml"));
+        Student student = (Student) container.getBean("stObj");
+        System.out.println(student);
+    }
+}
