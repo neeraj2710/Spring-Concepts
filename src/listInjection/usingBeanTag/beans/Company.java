ebc71d09a81c42a24cb01e496d810b8edfd990e3
neera <neerajwadhwaney2003@gmail.com>
2024-07-24 18:40:34 +0530

    21-07-2024

---
+++ src/listInjection/usingBeanTag/beans/Company.java
@@ -0,0 +1,36 @@
+package listInjection.usingBeanTag.beans;
+
+
+import java.util.List;
+
+public class Company {
+    private String name;
+    private List<Employee> employees;
+
+    public Company() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", employees=" + employees +
+                '}';
+    }
+}
